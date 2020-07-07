b57b4badbaa86631052ca5f4f20f5eb8b42314de
Evan Shortiss <evanshortiss@gmail.com>
2020-07-07 15:19:09 +0100

    fix: nodejs version comment

--- ftp/ftp.setup.sh
+++ ftp/ftp.setup.sh
@@ -4,7 +4,7 @@ SCRIPT_PARENT=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 echo "Installing Node.js v12"
 sleep 2
 
-# install node.js 2
+# install node.js 12
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 source ~/.bashrc
 nvm install 12
